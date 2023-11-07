08e44e9533baf76f0818d7b4bd94566c44181f6a
hung <hungbdhe163402@fpt.edu.vn>
2023-11-07 21:55:20 +0700

    banner

--- app/src/main/java/com/example/project_prm392/HomeFragment.java
+++ app/src/main/java/com/example/project_prm392/HomeFragment.java
@@ -1,6 +1,8 @@
 package com.example.project_prm392;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,8 +12,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.project_prm392.entity.Book;
+import com.example.project_prm392.entity.Photo;
 import com.example.project_prm392.entity.Tag;
 import com.example.project_prm392.entity.Tag;
 import com.google.firebase.database.DataSnapshot;
@@ -22,6 +26,10 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import me.relex.circleindicator.CircleIndicator;
 
 
 public class HomeFragment extends Fragment {
@@ -29,81 +37,113 @@ public class HomeFragment extends Fragment {
     private RecyclerView rcvCategory;
     private TagAdapter tagAdapter;
 
+    private ViewPager viewPager;
+    private CircleIndicator circleIndicator;
+    private PhotoAdapter photoAdapter;
+
+    private List<Photo> mListPhoto;
+    private Timer mTimer;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
         rcvCategory = view.findViewById(R.id.rcv_category);
-        tagAdapter = new TagAdapter(getContext());
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        viewPager = view.findViewById(R.id.viewpager);
+
+        circleIndicator = view.findViewById(R.id.circle_indicator);
+
+        mListPhoto = getListPhoto();
+
+        photoAdapter = new PhotoAdapter(requireContext(), mListPhoto);
+        viewPager.setAdapter(photoAdapter);
+        circleIndicator.setViewPager(viewPager);
+
+        tagAdapter = new TagAdapter(requireContext());
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(requireContext());
         rcvCategory.setLayoutManager(linearLayoutManager);
-        tagAdapter.setData(getListCategory());
         rcvCategory.setAdapter(tagAdapter);
+
+        getListCategory();
+
+        autoSlideImage();
+
         return view;
     }
 
-    private List<Tag> getListCategory() {
-        List<Tag> listCategory = new ArrayList<>();
+    private List<Photo> getListPhoto() {
+        List<Photo> list = new ArrayList<>();
+        list.add(new Photo(R.drawable.banner));
+        list.add(new Photo(R.drawable.banner2));
+        list.add(new Photo(R.drawable.banner3));
+        return list;
+    }
+
+
+    private void autoSlideImage(){
+        if(mListPhoto == null || mListPhoto.isEmpty() || viewPager == null ){
+            return;
+        }
+
+        if(mTimer ==null ){
+            mTimer = new Timer();
+        }
+
+        mTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                new Handler(Looper.getMainLooper()).post(new Runnable() {
+                    @Override
+                    public void run() {
+                        int currentItem = viewPager.getCurrentItem();
+                        int totalItem = mListPhoto.size() -1;
+                        if(currentItem<totalItem){
+                            currentItem++;
+                            viewPager.setCurrentItem(currentItem);
+
+                        }else {
+                            viewPager.setCurrentItem(0);
+                        }
+
+                    }
+                });
+            }
+        },500,3000);
+
+
+
+    }
+
+    private void getListCategory() {
         List<Book> listBook = new ArrayList<>();
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
         DatabaseReference databaseReference = firebaseDatabase.getReference();
 
         databaseReference.child("book").addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
+            public void onDataChange(DataSnapshot snapshot) {
                 listBook.clear();
                 for (DataSnapshot snap : snapshot.getChildren()) {
                     Book book = snap.getValue(Book.class);
                     listBook.add(book);
-                    listCategory.clear();
-                    listCategory.add(new Tag("cate1", listBook));
-                    listCategory.add(new Tag("cate2", listBook));
-                    listCategory.add(new Tag("cate3", listBook));
-                    listCategory.add(new Tag("cate4", listBook));
-                    listCategory.add(new Tag("cate5", listBook));
-                    listCategory.add(new Tag("cate6", listBook));
-                    listCategory.add(new Tag("cate7", listBook));
-                    listCategory.add(new Tag("cate8", listBook));
-                    listCategory.add(new Tag("cate9", listBook));
-                    listCategory.add(new Tag("cate10", listBook));
                 }
-                tagAdapter.notifyDataSetChanged();
+
+                List<Tag> listCategory = new ArrayList<>();
+                listCategory.add(new Tag("Giải trí", listBook));
+                listCategory.add(new Tag("Truyện", listBook));
+                listCategory.add(new Tag("Đời sống", listBook));
+                listCategory.add(new Tag("Thể thao", listBook));
+                listCategory.add(new Tag("Tiểu thuyết", listBook));
+
+                tagAdapter.setData(listCategory);
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
+            public void onCancelled(DatabaseError error) {
+                // Xử lý khi có lỗi
             }
-
         });
-        return listCategory;
     }
 }
-
-
-
-//        listBook.add(new Book(1,"Book1","Aaa","hung",1,"https://i.truyenvua.com/ebook/190x247/boku-no-hero-academia_1552459650.jpg?gt=hdfgdfg&mobile=2",1));
-//        listBook.add(new Book(2,"Book2","Aaa","hung",1,"https://i.truyenvua.com/ebook/190x247/boku-no-hero-academia_1552459650.jpg?gt=hdfgdfg&mobile=2",1));
-//        listBook.add(new Book(3,"Book3","Aaa","hung",1,"https://i.truyenvua.com/ebook/190x247/boku-no-hero-academia_1552459650.jpg?gt=hdfgdfg&mobile=2",1));
-//        listBook.add(new Book(4,"Book4","Aaa","hung",1,"https://i.truyenvua.com/ebook/190x247/boku-no-hero-academia_1552459650.jpg?gt=hdfgdfg&mobile=2",1));
-//        listBook.add(new Book(5,"Book5","Aaa","hung",1,"https://i.truyenvua.com/ebook/190x247/boku-no-hero-academia_1552459650.jpg?gt=hdfgdfg&mobile=2",1));
-//        listBook.add(new Book(6,"Book6","Aaa","hung",1,"https://i.truyenvua.com/ebook/190x247/boku-no-hero-academia_1552459650.jpg?gt=hdfgdfg&mobile=2",1));
-//        listBook.add(new Book(7,"Book7","Aaa","hung",1,"https://i.truyenvua.com/ebook/190x247/boku-no-hero-academia_1552459650.jpg?gt=hdfgdfg&mobile=2",1));
-//        listBook.add(new Book(8,"Book8","Aaa","hung",1,"https://i.truyenvua.com/ebook/190x247/boku-no-hero-academia_1552459650.jpg?gt=hdfgdfg&mobile=2",1));
-//        listBook.add(new Book(9,"Book9","Aaa","hung",1,"https://i.truyenvua.com/ebook/190x247/boku-no-hero-academia_1552459650.jpg?gt=hdfgdfg&mobile=2",1));
-//        listBook.add(new Book(10,"Book10","Aaa","hung",1,"https://i.truyenvua.com/ebook/190x247/boku-no-hero-academia_1552459650.jpg?gt=hdfgdfg&mobile=2",1));
-//
-//        listCategory.add(new Category("cate1", listBook));
-//        listCategory.add(new Category("cate2", listBook));
-//        listCategory.add(new Category("cate3", listBook));
-//        listCategory.add(new Category("cate4", listBook));
-//        listCategory.add(new Category("cate5", listBook));
-//        listCategory.add(new Category("cate6", listBook));
-//        listCategory.add(new Category("cate7", listBook));
-//        listCategory.add(new Category("cate8", listBook));
-//        listCategory.add(new Category("cate9", listBook));
-//        listCategory.add(new Category("cate10", listBook));
-//
-//
-//        return listCategory;
 
 
